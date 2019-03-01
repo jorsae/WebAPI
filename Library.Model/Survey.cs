4e4c679f45fb3cc8beee0d73cd0740387c24de59
WeeDLY <jorgen.sater@gmail.com>
2019-03-01 13:23:03 +0100

    Startup project, with simple test

---
+++ Library.Model/Survey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class Survey
+    {
+        private int id;
+        public int Id { get => id; set => id = value; }
+
+        private int userId;
+        public int UserId { get => userId; set => userId = value; }
+
+        private DateTime creationTime;
+        public DateTime CreationTime { get => creationTime; set => creationTime = value; }
+
+
+        private static int NumberOfSurveys = 0;
+
+        public Survey(int userId)
+        {
+            Id = NumberOfSurveys++;
+            UserId = userId;
+            CreationTime = DateTime.Now;
+        }
+    }
+}
---
+++ WebAPI.Tests/SurveyTests.cs
@@ -0,0 +1,30 @@
+﻿using Library.Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Tests
+{
+    [TestFixture]
+    public class SurveyTests
+    {
+        private Survey survey;
+
+        [SetUp]
+        public void SetupBeforeEachTest()
+        {
+            survey = new Survey(1);
+        }
+
+        [Test]
+        public void Assert_survey_not_same_id()
+        {
+            Survey survey2 = new Survey(1);
+
+            Assert.AreNotEqual(survey.Id, survey2.Id);
+        }
+    }
+}
