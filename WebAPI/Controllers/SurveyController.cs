0cfc8e5d8a12c87959c58f0fafc98a083fd777ef
WeeDLY <jorgen.sater@gmail.com>
2019-03-12 12:37:17 +0100

    Able to add Survey to database.

--- WebAPI/Controllers/SurveyController.cs
+++ WebAPI/Controllers/SurveyController.cs
@@ -15,6 +15,7 @@ namespace WebAPI.Controllers
     public class SurveyController : ApiController
     {
         private LibraryContext db = new LibraryContext();
+
         // GET: api/Survey
         public IQueryable<Survey> GetSurveys()
         {
@@ -25,10 +26,46 @@ namespace WebAPI.Controllers
         [Route("api/Survey/{id}")]
         [HttpGet]
         [ResponseType(typeof(Survey))]
-        public async Task<IHttpActionResult> GetRegisterUser(int id)
+        public async Task<IHttpActionResult> GetSurvey(int id)
         {
             Survey survey = await db.Surveys.FindAsync(id);
             return Ok(survey);
+        }
+
+        // PUT: api/Survey/{survey}
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutSurvey(Survey survey)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Surveys.Add(survey);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SurveyExists(survey.Id))
+                    return NotFound();
+                else
+                    return StatusCode(HttpStatusCode.InternalServerError);
+            }
+            catch (Exception)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError);
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        private bool SurveyExists(int id)
+        {
+            return db.Surveys.Count(survey => survey.Id == id) > 0;
         }
     }
 }
