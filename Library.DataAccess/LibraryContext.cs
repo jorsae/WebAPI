21ebdb2b7024cc66895a61190c17c77e6bc9761b
WeeDLY <jorgen.sater@gmail.com>
2019-03-19 11:39:18 +0100

    Added new database connection string

--- Library.DataAccess/LibraryContext.cs
+++ Library.DataAccess/LibraryContext.cs
@@ -18,7 +18,7 @@ namespace Library.DataAccess
         public LibraryContext()
         {
             Configuration.ProxyCreationEnabled = false;
-            string connectionString = "Server = tcp:bachelorsurvey.database.windows.net,1433; Initial Catalog = BachelorDB; Persist Security Info = False; User ID = bo19funky; Password = 1elvroijwnv%!\"rnåu9vnu59+2; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+            string connectionString = "Server=tcp:bo19.database.windows.net,1433;Initial Catalog=BachDB;Persist Security Info=False;User ID=bo19;Password=¤\"%Vvnmi03104g9h12fv345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             Database.Connection.ConnectionString = connectionString;
             Database.SetInitializer(new LibraryDBInitializer());
         }
