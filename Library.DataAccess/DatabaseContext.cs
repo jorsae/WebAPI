0e5dd871038fa4c1992b8877c5b7c2951062c81d
WeeDLY <jorgen.sater@gmail.com>
2019-04-10 15:31:25 +0200

    Removed hardcoded connection string in DatabaseContext.(for debugging)

--- Library.DataAccess/DatabaseContext.cs
+++ Library.DataAccess/DatabaseContext.cs
@@ -1,11 +1,5 @@
 ﻿using Library.Model;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Library.DataAccess
 {
@@ -18,8 +12,6 @@ namespace Library.DataAccess
         public DatabaseContext()
         {
             Configuration.ProxyCreationEnabled = false;
-            Database.Connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WebApi_TestDatabase;Integrated Security=True;Pooling=False";
-            Database.CreateIfNotExists();
             Database.SetInitializer(new DatabaseInitializer());
         }
     }
