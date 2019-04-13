1c70c0b6d41df31d57198ccbd86e815b0a70eaee
WeeDLY <jorgen.sater@gmail.com>
2019-04-13 22:28:00 +0200

    Fixed setup for SurveyQuestionController tests. Added tests for get surveyquestion. Added test for get surveyquestion by surveyid

--- WebAPI.Tests/SurveyQuestionControllerTests.cs
+++ WebAPI.Tests/SurveyQuestionControllerTests.cs
@@ -3,8 +3,6 @@ using Library.Model;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Results;
@@ -17,26 +15,27 @@ namespace WebAPI.Tests
     {
         private DatabaseContext context;
         private SurveyQuestionController sqController;
+        private SurveyController surveyController;
         private SurveyQuestion surveyQuestion;
         private Survey survey;
 
         [OneTimeSetUp]
-        public async Task OneTimeSetUp()
+        public void OneTimeSetUp()
         {
             context = new DatabaseContext();
             context.Database.Connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WebApi_TestDatabase2;Integrated Security=True;Pooling=False";
             context.Database.CreateIfNotExists();
-
-            survey = new Survey("test survey");
-            SurveyController surveyController = new SurveyController();
-            await surveyController.PutSurvey(survey);
         }
 
         [SetUp]
-        public void SetUp()
+        public async Task SetUp()
         {
-            surveyQuestion = new SurveyQuestion(survey.SurveyId, 1, "test question");
+            surveyController = new SurveyController();
             sqController = new SurveyQuestionController();
+
+            survey = new Survey("test survey");
+            await surveyController.PutSurvey(survey);
+            surveyQuestion = new SurveyQuestion(survey.SurveyId, 1, "test question");
         }
 
         [Test]
@@ -52,5 +51,37 @@ namespace WebAPI.Tests
             IHttpActionResult result = await sqController.PutSurveyQuestion(new SurveyQuestion(1000, 1, "question"));
             Assert.IsNotInstanceOf(typeof(OkNegotiatedContentResult<SurveyQuestion>), result);
         }
-    }
+
+        [Test]
+        public async Task Assert_get_surveyquestion()
+        {
+            Console.WriteLine(surveyQuestion.SurveyId);
+            IHttpActionResult result = await sqController.PutSurveyQuestion(surveyQuestion);
+            Assert.IsInstanceOf(typeof(OkNegotiatedContentResult<SurveyQuestion>), result);
+
+            IHttpActionResult testResult = await sqController.GetSurveyQuestion(surveyQuestion.SurveyQuestionId);
+            Assert.IsInstanceOf(typeof(OkNegotiatedContentResult<SurveyQuestion>), testResult);
+        }
+
+        [Test]
+        public async Task Assert_get_surveyquestion_by_surveyid()
+        {
+            IHttpActionResult result1 = await sqController.PutSurveyQuestion(surveyQuestion);
+            Assert.IsInstanceOf(typeof(OkNegotiatedContentResult<SurveyQuestion>), result1);
+            IHttpActionResult result2 = await sqController.PutSurveyQuestion(new SurveyQuestion(survey.SurveyId, 1, "asd"));
+            Assert.IsInstanceOf(typeof(OkNegotiatedContentResult<SurveyQuestion>), result2);
+
+            IHttpActionResult testResult = await sqController.GetSurveyQuestionBySurveyId(surveyQuestion.SurveyId);
+            Assert.IsInstanceOf(typeof(OkNegotiatedContentResult<List<SurveyQuestion>>), testResult);
+        }
+
+        [Test]
+        public async Task Assert_sets_question_number_to_1()
+        {
+            IHttpActionResult result = await sqController.PutSurveyQuestion(new SurveyQuestion(survey.SurveyId, 4000, "a"));
+            var contentResult = result as OkNegotiatedContentResult<SurveyQuestion>;
+            SurveyQuestion sq = contentResult.Content;
+            Assert.AreEqual(1, sq.QuestionNumber);
+        }
+    } 
 }
