358c4c25e9323d1bece0715c4d6104c002e17a2d
WeeDLY <jorgen.sater@gmail.com>
2019-04-13 15:55:05 +0200

    Moved tests purely on Survey model to SurveyTests.cs

---
+++ WebAPI.Tests/SurveyControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Library.DataAccess;
+using Library.Model;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Web.Http;
+using System.Web.Http.Results;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests
+{
+    [TestFixture]
+    public class SurveyControllerTests
+    {
+        private DatabaseContext context;
+        private SurveyController surveyController;
+        private Survey survey;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            context = new DatabaseContext();
+            context.Database.Connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WebApi_TestDatabase2;Integrated Security=True;Pooling=False";
+            context.Database.CreateIfNotExists();
+        }
+
+        [SetUp]
+        public void SetupBeforeEachTest()
+        {
+            survey = new Survey("Test");
+            surveyController = new SurveyController();
+        }
+
+        [Test]
+        public async Task Assert_put_survey()
+        {
+            IHttpActionResult actionResult = await surveyController.PutSurvey(survey);
+            var contentResult = actionResult as OkNegotiatedContentResult<Survey>;
+            Assert.IsNotNull(contentResult);
+        }
+
+        [Test]
+        public async Task Assert_get_survey_by_id()
+        {
+            IHttpActionResult actionResult = await surveyController.PutSurvey(survey);
+            var contentResult = actionResult as OkNegotiatedContentResult<Survey>;
+            Assert.IsNotNull(contentResult);
+
+            IHttpActionResult result = await surveyController.GetSurveyById(survey.SurveyId);
+            var getContentResult = actionResult as OkNegotiatedContentResult<Survey>;
+            Assert.NotNull(getContentResult);
+        }
+    }
+}
--- WebAPI.Tests/SurveyTests.cs
+++ WebAPI.Tests/SurveyTests.cs
@@ -3,7 +3,6 @@ using Library.Model;
 using NUnit.Framework;
 using System;
 using System.Threading.Tasks;
-using System.Transactions;
 using System.Web.Http;
 using System.Web.Http.Results;
 using WebAPI.Controllers;
@@ -13,23 +12,12 @@ namespace WebAPI.Tests
     [TestFixture]
     public class SurveyTests
     {
-        private DatabaseContext context;
-        private SurveyController surveyController;
         private Survey survey;
-
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            context = new DatabaseContext();
-            context.Database.Connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WebApi_TestDatabase2;Integrated Security=True;Pooling=False";
-            context.Database.CreateIfNotExists();
-        }
 
         [SetUp]
         public void SetupBeforeEachTest()
         {
             survey = new Survey("Test");
-            surveyController = new SurveyController();
         }
 
         [Test]
@@ -37,26 +25,6 @@ namespace WebAPI.Tests
         {
             DateTime newDate = survey.CreationDate.AddDays(7);
             Assert.True(newDate.Equals(survey.ClosingDate));
-        }
-
-        [Test]
-        public async Task Assert_put_survey()
-        {
-            IHttpActionResult actionResult = await surveyController.PutSurvey(survey);
-            var contentResult = actionResult as OkNegotiatedContentResult<Survey>;
-            Assert.IsNotNull(contentResult);
-        }
-
-        [Test]
-        public async Task Assert_get_survey_by_id()
-        {
-            IHttpActionResult actionResult = await surveyController.PutSurvey(survey);
-            var contentResult = actionResult as OkNegotiatedContentResult<Survey>;
-            Assert.IsNotNull(contentResult);
-
-            IHttpActionResult result = await surveyController.GetSurveyById(survey.SurveyId);
-            var getContentResult = actionResult as OkNegotiatedContentResult<Survey>;
-            Assert.NotNull(getContentResult);
         }
     }
 }
